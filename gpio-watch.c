60eafd4d4d8c24db71b3374717a0f11585ebaeff
Lars Kellogg-Stedman <lars@redhat.com>
2014-07-08 12:01:33 -0400

    basic functionality works
    with this commit, gpio-watch will wait for events and run
    scripts in response.  It's not pretty and doesn't really have much in
    the way of error checking.

--- gpio-watch.c
+++ gpio-watch.c
@@ -1,14 +1,14 @@
 #include <assert.h>
+#include <fcntl.h>
+#include <getopt.h>
+#include <poll.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <getopt.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
 #include <string.h>
-
-#include <poll.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 #define EDGE_RISING 0
 #define EDGE_FALLING 1
@@ -37,6 +37,18 @@ struct pin {
 struct pin *pins = NULL;
 int num_pins = 0;
 
+int is_file (const char *path) {
+	struct stat buf;
+	int err;
+
+	err = stat(path, &buf);
+	if (-1 == err) {
+		return 0;
+	}
+
+	return S_ISREG(buf.st_mode);
+}
+
 int is_dir (const char *path) {
 	struct stat buf;
 	int err;
@@ -60,7 +72,7 @@ int parse_edge(const char *edge) {
 		return -1;
 }
 
-int pin_export(int pin) {
+void pin_export(int pin) {
 	char *export_path;
 	char *pin_path;
 
@@ -110,6 +122,91 @@ int pin_set_edge(int pin, int edge) {
 		fprintf(fp, "both\n");
 	fclose(fp);
 	free(pin_path);
+}
+
+void run_script (int pin, int value) {
+	char *script_path,
+	     pin_str[GPIODIRLEN],
+	     value_str[2];
+	int script_path_len;
+	pid_t pid;
+	int status;
+
+	script_path_len = strlen(script_dir)
+			+ GPIODIRLEN
+			+ 2;
+	script_path = (char *)malloc(script_path_len);
+
+	snprintf(script_path, script_path_len,
+			"%s/%d", script_dir, pin);
+
+	if (! is_file(script_path)) {
+		fprintf(stderr, "error: script \"%s\" does not exist\n",
+				script_path);
+		return;
+	}
+
+	snprintf(pin_str, GPIODIRLEN, "%d", pin);
+	sprintf(value_str, "%d", value);
+
+	if (0 == (pid = fork())) {
+		int res;
+		res = execl(script_path, script_path, pin_str, value_str, (char *)NULL);
+		if (-1 == res) exit(255);
+	}
+
+	wait(&status);
+
+	free(script_path);
+
+}
+
+int watch_pins() {
+	struct pollfd *fdlist;
+	int i;
+	char *pin_path;
+	int pin_path_len;
+	char valbuf[3];
+
+	valbuf[2] = '\0';
+
+	pin_path_len = strlen(GPIO_BASE) + GPIODIRLEN + strlen("value") + 3;
+	pin_path = (char *)malloc(pin_path_len);
+
+	fdlist = (struct pollfd *)malloc(sizeof(struct pollfd) * num_pins);
+	for (i=0; i<num_pins; i++) {
+		int fd;
+
+		snprintf(pin_path, pin_path_len,
+				"%s/gpio%d/value", GPIO_BASE, pins[i].pin);
+		fd = open(pin_path, O_RDONLY);
+//		read(fd, valbuf, 2);
+		fdlist[i].fd = fd;
+		fdlist[i].events = POLLPRI;
+	}
+
+	while (1) {
+		int err;
+
+		err = poll(fdlist, num_pins, -1);
+		if (-1 == err) {
+			perror("poll");
+			exit(1);
+		}
+
+		for (i=0; i<num_pins; i++) {
+			printf("%d %d %d\n",
+					pins[i].pin,
+					fdlist[i].fd,
+					fdlist[i].revents);
+			if (fdlist[i].revents & POLLPRI) {
+				lseek(fdlist[i].fd, 0, SEEK_SET);
+				read(fdlist[i].fd, valbuf, 2);
+				run_script(pins[i].pin,
+						valbuf[0] == '1' ? 1 : 0);
+			}
+		}
+	}
 }
 
 int main(int argc, char **argv) {
@@ -172,6 +269,8 @@ int main(int argc, char **argv) {
 		pin_export(pins[i].pin);
 		pin_set_edge(pins[i].pin, pins[i].edge);
 	}
+
+	watch_pins();
 
 	return 0;
 }
